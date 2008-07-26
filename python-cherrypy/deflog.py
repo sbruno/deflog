18ea26e30fac4f824d18182859ab77038eda6ec1
bananabruno <bananabruno@bbcd7327-0f4a-0410-9f82-1b8ed3c9a822>
2008-07-26 21:05:55 +0000

    Primer intento por traducir una pagina, parseando el HTML a los ponchazos

--- python-cherrypy/deflog.py
+++ python-cherrypy/deflog.py
@@ -13,7 +13,7 @@ except ImportError:
     import sys
     sys.exit("Error importando cherrypy. Comporobar su correcta instalación. http://www.cherrypy.org/")
 import re
-
+from urllib import urlopen
 
 def tohtml(word):
     """ \r\n -> <br/> """
@@ -461,6 +461,35 @@ def fixmissingvowels(word):
     return word
 
 
+def desfotologuear(text, deleetval, desalternarval, desmultiplicarval, desmsarval, desestupidizarval, deszezearval, deskarval, desporteniarval, fixmissingvowelsval):
+    words = re.split(u"([\\w\\dáéíóúñ+]+)",dessimbolizar(text))
+    
+    html = ""
+
+    if words:
+        words = map(tohtml, words)
+        if deleetval == 'on':
+            words = map(deleet, words)
+        if desalternarval == 'on':
+            words = map(desalternar, words)
+        if desmultiplicarval == 'on':
+            words = map(desmultiplicar, words)
+        if desmsarval == 'on':
+            words = map(desms, words)
+        if desestupidizarval == 'on':
+            words = map(desestupidizar, words)
+        if deszezearval == 'on':
+            words = map(deszezear, words)
+        if deskarval == 'on':
+            words = map(desk, words)
+        if desporteniarval == 'on':
+            words = map(desporteniar, words)
+        if fixmissingvowelsval == 'on':
+            words = map(fixmissingvowels, words)
+        html += "".join( [ w for w in words] ) 
+    return html
+
+
 
 class Desfotologueador:
 
@@ -704,6 +733,102 @@ class Desfotologueador:
         </table>\
     </form>\
 \
+<h4>Traducir una p&aacute;gina web</h4>\
+<form id="web_form" action="/translate">\
+    <table id="webtable">\
+        <tbody>\
+            <tr>\
+                <td width="100%">\
+                    <input type="text" tabindex="0" dir="ltr" value="http://" id="url" name="u"/>\
+                </td>\
+            </tr>\
+            <tr>\
+            <td id="submitcell">\
+                <table>\
+\
+                <tr>\
+\
+                <td id="selectcell">\
+                    <select name="langpair">\
+                        <option value="flog|es">Fotolog a Espa&ntilde;ol</option>\
+                    </select>\
+                </td>\
+                <td>\
+                    <input type="submit" value="Traducir"/>\
+                </td>\
+\
+                </tr>\
+\
+                <tr>\
+                <td class="select_metodos_title"> M&eacute;todos a aplicar al texto:</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Desmultiplicar</td>\
+                <td><input type="radio" name="desmultiplicarval" value="on" ' + desmultiplicar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="desmultiplicarval" value="off" ' + desmultiplicar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Deszezear</td>\
+                <td><input type="radio" name="deszezearval" value="on" ' + deszezear_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="deszezearval" value="off" ' + deszezear_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Des-k-ar</td>\
+                <td><input type="radio" name="deskarval" value="on" ' + deskar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="deskarval" value="off" ' + deskar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>DesSMSar</td>\
+                <td><input type="radio" name="desmsarval" value="on" ' + desmsar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="desmsarval" value="off" ' + desmsar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Desestupidizar</td>\
+                <td><input type="radio" name="desestupidizarval" value="on" ' + desestupidizar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="desestupidizarval" value="off" ' + desestupidizar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Desalternar</td>\
+                <td><input type="radio" name="desalternarval" value="on" ' + desalternar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="desalternarval" value="off" ' + desalternar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Desporteñar</td>\
+                <td><input type="radio" name="desporteniarval" value="on" ' + desporteniar_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="desporteniarval" value="off" ' + desporteniar_checked_no + ' > No</td>\
+                </tr>\
+\
+                <tr>\
+                <td>Deleet</td>\
+                <td><input type="radio" name="deleetval" value="on" ' + deleet_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="deleetval" value="off" ' + deleet_checked_no + ' > No</td>\
+                </tr>\
+\
+\
+                <tr>\
+                <td>Fix missing vowels</td>\
+                <td><input type="radio" name="fixmissingvowelsval" value="on" ' + fixmissingvowels_checked_yes + ' > Si</td>\
+                <td><input type="radio" name="fixmissingvowelsval" value="off" ' + fixmissingvowels_checked_no + ' > No</td>\
+                </tr>\
+\
+\
+                </table>\
+            </td>\
+\
+            <td>\
+            </td>\
+            </tr>\
+        </tbody>\
+    </table>\
+</form>\
+\
     <div class="footer">\
     <a href="http://www.santiagobruno.com.ar/programas.html#deflog">More info on my website</a> - <a href="http://cervezaconlupines.blogspot.com/2008/03/presentando-deflog.html">About This Shit</a> - <a href="index?text=LocURaAAaaA%21%21%21%0D%0Ake+loko+estoooo+mIeRdA%21%21%21%0D%0A%0D%0AAaAaAAAaaAiiiii++firmeeennnn+leeemmmddoooo%21%21%21%0D%0A%0D%0Anuc+k+pasa+ak.+t+voi+a+ver+dsp.%0D%0Aqe+andes+d+mil%21%21%0D%0AbesOtte%0D%0A%0D%0Aesto+q+m+dijistes+ta+groxo+maallll+grax+xq+m+dijistessss+cdo+lo+vistessssss%0D%0A%0D%0Atoy+reeee+lokooo+blds%21%21%0D%0Aaahhrrreee%0D%0At+qiero%2C+we%2C+chauuuu%21%21%21%0D%0Abss.%0D%0Aazi+ez+ezto%2C+nos+vems%2C+stamos.+dspu%E9s+dcime.+4gu4n73+a77aqu3%0D%0Ajkajkajkaajkajk">Translation Example</a><br/><br/>This site is not affiliated with Google and is not against floggers or any internet community<br/><br/>©2008 Santiago Bruno</div>\
 \
@@ -712,7 +837,68 @@ class Desfotologueador:
                 
         return html
 
+
+    def translate(self, u = "", desmultiplicarval = 'on', deszezearval = 'off', deskarval = 'off', desmsarval = 'on', desestupidizarval = 'on', desalternarval = 'on',  desporteniarval = 'on', deleetval = "on", fixmissingvowelsval = "on", **kwargs):
+
+        try:
+            url = urlopen(u)
+            archivo = url.read()
+        except:
+            return ""
+        difmay = 0
+        to_translate = " "
+        tag = ""
+        nottrans = 0
+
+        result = ""
+
+        i = 0
+        while (i < len(archivo)):
+        
+            read = archivo[i]
+        
+            if (difmay == 0 and read != '<' and read != '>'):
+                to_translate += read
+            elif (difmay == 0 and read == '<'):
+                tag = ""
+                if (nottrans == 0):
+                    result += desfotologuear(to_translate, deleetval, desalternarval, desmultiplicarval, desmsarval, desestupidizarval, deszezearval, deskarval, desporteniarval, fixmissingvowelsval)
+                else:
+                    result += to_translate
+
+                to_translate = " "
+                difmay = 1
+                result += read
+            elif (read == '<'):
+                result += read
+                difmay += 1
+                tag = ""
+            elif (read == '>'):
+                result += tag
+                result += read
+                difmay -= 1
+                
+                if (nottrans == 0 and (difmay == 0) and ((tag.startswith("style")) or (tag.startswith("script")))):
+                    nottrans += 1
+                    tag = ""
+                elif ((nottrans > 0) and (difmay == 0) and ((tag.startswith("/style")) or (tag.startswith("/script")))):
+                    nottrans -= 1
+                    tag = ""
+        
+                tag = ""
+        
+            elif (difmay > 0):
+                tag += read
+            
+            i = i + 1
+
+        return result
+
+
+
+
     index.exposed = True
+    translate.exposed = True
 
 cherrypy.tree.mount(Desfotologueador())
 
